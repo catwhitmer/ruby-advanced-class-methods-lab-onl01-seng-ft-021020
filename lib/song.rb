7a7bee7269df90ae0f242c8e6d988071b3457220
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-22 14:16:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,10 +49,7 @@ class Song
   end
   
   def self.new_from_filename(filename)
-    song = self.new
-    song.name = filename.split(".mp3")[1]
-    song.artist_name = filename.split(" - ")[0]
-    song
+ 
   end
   
   def self.create_from_filename(filename)
